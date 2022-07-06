733fe35ed0d7e3bf1c23df8eb169023e4f4c4513
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-06 10:51:57 +0200

    update API_URL with new api url

--- src/stores/api_url.jsx
+++ src/stores/api_url.jsx
@@ -1,4 +1,4 @@
 // API production environment URL
-export const API_URL = 'https://sharcade.herokuapp.com/'
+export const API_URL = 'https://sharcade-api.herokuapp.com/'
 // API development & test environment URL
 //export const API_URL = 'http://localhost:3000/'
