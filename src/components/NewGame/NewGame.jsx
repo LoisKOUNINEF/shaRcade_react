d8a2b4de4c872293b621fe6b3c1caf6b80409da3
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-18 13:00:50 +0200

    first try for game type choice on game creation

--- src/components/NewGame/NewGame.jsx
+++ src/components/NewGame/NewGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { authorizationAtom } from '../../stores/cookies';
 import Form from 'react-bootstrap/Form';
@@ -11,7 +11,9 @@ const NewGame = () => {
   const [gameTitle, setGameTitle] = useState();
   const [gameUrl, setGameUrl] = useState();
   const [gameDescription, setGameDescription] = useState();
-  const [gameType, setGameType] = useState(1);
+  const [gameTypesList, setGameTypesList] = useState([]);
+  const [gameTypesLoading, setGameTypesLoading] = useState(true);
+  const [gameType, setGameType] = useState();
   const [imageUrl, setImageUrl] = useState();
   const [mobileReady, setMobileReady] = useState(false);
   const [myAuthorization, setAuthorization] = useAtom(authorizationAtom);
@@ -24,7 +26,20 @@ const NewGame = () => {
     // game_type_id: integer
     // mobile_ready : boolean
     // image_url: string
-
+    useEffect(() => {
+      fetch(`${API_URL}game_types`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        }
+      })
+      .then((response) => response.json())
+      .then((response) => {
+        setGameTypesList(response);
+        setGameTypesLoading(response.length <= 0);
+      });
+    }, [gameTypesLoading]);
     const submitData = (e) => {
 
       e.preventDefault();
@@ -54,37 +69,41 @@ const NewGame = () => {
 
     return (
       <div className="new-game-form-container">
-        <Form className="form" onSubmit={submitData}>
-          <Form.Group className="mb-3" controlId="formSubmitGame">
-            <Form.Label className="label">Game name</Form.Label>
-            <Form.Control className="field" type="input" placeholder="Enter a game name" onChange={(e) => setGameTitle(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formSubmitGame">
-            <Form.Label className="label">Game link</Form.Label>
-            <Form.Control className="field" type="input" placeholder="Set link for our game" onChange={(e) => setGameUrl(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formSubmitGame">
-            <Form.Label className="label">Game description</Form.Label>
-            <Form.Control className="field" type="textarea" placeholder="Describe your game" onChange={(e) => setGameDescription(e.target.value)} />
-          </Form.Group>
-{/*       <Form.Group className="mb-3" controlId="formSubmitGame">
-            <Form.Label className="label">Game type</Form.Label>
-            <Form.Control className="field" type="integer" placeholder="Select a category for your game" onChange={(e) => setGameType(e.target.value)} />
-          </Form.Group>*/}
-          <Form.Group className="mb-3" controlId="formSubmitGame">
-            <Form.Label className="label">Game image URL</Form.Label>
-            <Form.Control className="field" type="input" placeholder="Set an image for your game" onChange={(e) => setImageUrl(e.target.value)} />
-          </Form.Group>
+      <Form className="form" onSubmit={submitData}>
+      <Form.Group className="mb-3" controlId="formSubmitGame">
+      <Form.Label className="label">Game name</Form.Label>
+      <Form.Control className="field" type="input" placeholder="Enter a game name" onChange={(e) => setGameTitle(e.target.value)} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formSubmitGame">
+      <Form.Label className="label">Game link</Form.Label>
+      <Form.Control className="field" type="input" placeholder="Set link for your game" onChange={(e) => setGameUrl(e.target.value)} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formSubmitGame">
+      <Form.Label className="label">Game description</Form.Label>
+      <Form.Control className="field" type="textarea" placeholder="Describe your game" onChange={(e) => setGameDescription(e.target.value)} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formSubmitGame">
+      <Form.Label className="label">Game type</Form.Label>
+      <select onChange={(e) => setGameType(e.target.value)}>
+      {gameTypesList.map((gametype) => (
+        <option value={gametype.id}>{gametype.game_type_title}</option>
+        ))}
+      </select>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formSubmitGame">
+      <Form.Label className="label">Game image URL</Form.Label>
+      <Form.Control className="field" type="input" placeholder="Set an image for your game" onChange={(e) => setImageUrl(e.target.value)} />
+      </Form.Group>
 {/*       <Form.Group className="mb-3" controlId="formSubmitGame">
             <Form.Label className="label">Playable on mobile ?</Form.Label>
             <Form.Control className="field" type="checkbox" placeholder="Is your game playable on mobile ?" onChange={(e) => setMobileReady(!mobileReady)} />
           </Form.Group>*/}
           <Button className="submit-btn" variant="primary" type="submit">
-            Submit
+          Submit
           </Button>
-        </Form>
-      </div>
-    );
+          </Form>
+          </div>
+          );
   }
 
   export default NewGame
