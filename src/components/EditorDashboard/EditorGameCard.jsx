3b2e096478146c1facb54d76acde91298d6d0d60
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-27 23:18:33 +0200

    add delete game btn, delete api_key manually. Have to add dependent: destroy to api to be able to remove this manual deletion.

--- src/components/EditorDashboard/EditorGameCard.jsx
+++ src/components/EditorDashboard/EditorGameCard.jsx
@@ -1,11 +1,38 @@
+import { API_URL } from '../../stores/api_url';
+
 function EditorGameCard(props) {
+
+  const submitData1 = () => {
+
+    fetch(`${API_URL}api_calls/${props.apikey.id}`, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    })
+    .catch((error) => console.log(error));
+  }
+
+  const submitData = () => {
+
+    fetch(`${API_URL}games/${props.game.id}`, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+    })
+    .catch((error) => console.log(error));
+  }
 
   return (
     <div className="editor-game-card">
       <h4>{props.game.game_title.normalize("NFD").replace(/[\u0300-\u036f]/g, "")}</h4>
       <p>APIKey : <span className="apikey">{props.apikey.api_key}</span></p>
+      <button onClick={() => {submitData1(); submitData();}}>Delete Game</button>
     </div>
-   );
+    );
 
 }
 
