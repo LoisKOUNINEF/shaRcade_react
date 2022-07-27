c05beecc9ccf664bb1b9ceaef10e8006ab3b4975
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-28 01:28:44 +0200

    remove manual apikey deletion

--- src/components/EditorDashboard/EditorGameCard.jsx
+++ src/components/EditorDashboard/EditorGameCard.jsx
@@ -2,18 +2,6 @@ import { API_URL } from '../../stores/api_url';
 
 function EditorGameCard(props) {
 
-  const submitData1 = () => {
-
-    fetch(`${API_URL}api_calls/${props.apikey.id}`, {
-      method: 'delete',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-    })
-    .catch((error) => console.log(error));
-  }
-
   const submitData = () => {
 
     fetch(`${API_URL}games/${props.game.id}`, {
@@ -30,7 +18,7 @@ function EditorGameCard(props) {
     <div className="editor-game-card">
       <h4>{props.game.game_title.normalize("NFD").replace(/[\u0300-\u036f]/g, "")}</h4>
       <p>APIKey : <span className="apikey">{props.apikey.api_key}</span></p>
-      <button onClick={() => {submitData1(); submitData();}}>Delete Game</button>
+      <button onClick={() => submitData()}>Delete Game</button>
     </div>
     );
 
