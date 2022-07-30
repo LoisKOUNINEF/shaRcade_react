3a2a044282ff951892cd4b5abec1cea3006bd724
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-30 12:56:48 +0200

    clearer hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,11 +4,18 @@ import './Hero.css';
 const Hero = () => {
     return (
         <section className='hero-container retro'>
-            <h1>Just <a href="/signup">shaRcade</a> it!</h1>
+            <h1>Just shaRcade it !</h1>
+            <div className="hero-txt">
+            <div className="hero-txt-left">
             <p>Enjoy <strong>coding</strong> browser games ?</p>
             <p>shaRcade is the place to Share'em all !</p>
+            </div>
+            <div className="hero-txt-right">
             <p>Enjoy <strong>playing</strong> browser games ?</p>
             <p>shaRcade is the place to Find'em all !</p>
+            </div>
+            </div>
+            <h4><a href="/signup">Join shaRcade Now !</a></h4>
         </section>
     )
 }
