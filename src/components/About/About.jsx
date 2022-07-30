8301c6725efc5ae2dccd148081bc073faa9e03db
LoisKOUNINEF <lkouninef@gmail.com>
2022-07-30 16:01:20 +0200

    more obvious expected data types

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,15 +8,15 @@ const About = () => {
         <h3>Want to link your game to shaRcade so that the whole world could enjoy it ?</h3>
         <p>It's just a coppel lines away !<br/>
         You will be provided an unique API Key when you register a game.<br/>
-        Here's a small exemple in JavaScript on how to send scores from your game to shaRcade :
+        Here's a small JavaScript snippet on how to send scores from your game to shaRcade :
         </p>
         <code>
         {`function submitScore() {`}<br/><br/>
         {`const data = {`}<br/>
         {`"score_token" : {`}<br/>
-        {`"hi_score" : Integer,`}<br/>
+        {`"hi_score" : INTEGER,`}<br/>
         {`"api_key" : YOUR_GAME'S_API_KEY,`}<br/>
-        {`"user_email" : String`}<br/>
+        {`"user_email" : STRING`}<br/>
         {`}`}<br/>
         {`};`}<br/><br/>
         {`fetch('https://sharcade-api.herokuapp.com/sharcade_api', {`}<br/>
